459e59180e615fc3a607e6d3b2ed403b7933cbb4
basheershalabi123 <62907868+basheershalabi123@users.noreply.github.com>
2025-03-06 19:58:42 +0200

    video

--- Web-fundamentals/JS/Video/script.js
+++ Web-fundamentals/JS/Video/script.js
@@ -1,15 +1,29 @@
 console.log("page loaded...");
 
-
+var video = document.querySelector("figure video")
 
 function showSettings(element,val){
     element.controls = val
 }
 
+
+
 function doPlay(element){
     element.play()
+    element.muted = false
+    element.autoplay = true
 }
 function doPause(element){
     element.pause()
     element.muted = true
+}
+
+video.onmouseover = function(){
+    showSettings(video,true)
+    doPlay(video);
+}
+
+video.onmouseleave = function(){
+    showSettings(video,false);
+    doPause(video)
 }

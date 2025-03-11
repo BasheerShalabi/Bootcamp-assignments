2806cfda3c18157b28cf0a48cae76f7a8eb1b709
basheershalabi123 <62907868+basheershalabi123@users.noreply.github.com>
2025-03-11 15:53:26 +0200

    calculator

---
+++ Web-fundamentals/JS/Calculator/script.js
@@ -0,0 +1,45 @@
+
+var expression =""
+var display=document.getElementById("display")
+var isInOp=false
+var font =40
+
+function press(num){
+   
+     if(num=='.' && !display.innerText.includes('.')){
+         display.innerText=`${display.innerText}${num}`
+     }else if(isInOp){
+        display.innerText=`${num}`
+        isInOp=false
+     }
+    else if(display.innerText=="0" || display.innerText=="NaN" || display.innerText=="undifined" || display.innerText=="Infinity"){
+        display.innerText=`${num}`
+    }else if(num!='.'){
+        display.innerText=`${display.innerText}${num}`
+    }
+
+console.log(isInOp)
+}
+
+function clr(){
+    display.innerText="0"
+    expression="0"
+    
+}
+
+function setOP(op){
+    expression = `${display.innerText}${op}`
+    isInOp=true
+    
+}
+
+function calculate(){
+    
+    if(!(expression.includes('+') || expression.includes('-') || expression.includes('*') || expression.includes('/'))) return
+    expression=`${expression}${display.innerText}`
+   display.innerText=eval(expression).toFixed(3)
+   console.log(expression)
+   
+   expression=""
+}
+var container = document.getElementById("expretion")

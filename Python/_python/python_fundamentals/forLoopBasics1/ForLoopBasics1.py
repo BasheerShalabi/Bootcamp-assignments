3aa2c5d890388deb83f35c2ba49af3dd65c72d15
basheershalabi123 <62907868+basheershalabi123@users.noreply.github.com>
2025-03-23 19:09:54 +0200

    forloopbasics

---
+++ Python/_python/python_fundamentals/forLoopBasics1/ForLoopBasics1.py
@@ -0,0 +1,41 @@
+
+
+for i in range(0,150):
+    print(i)
+
+print("-----------------------------------------")
+
+for i in range(5,1000,5):
+    print(i)
+
+print("-----------------------------------------")
+
+for i in range(1,100):
+    if i%10 is 0:
+        print("Coding Dojo")
+    elif i%5 is 0:
+        print("Coding")
+    else:
+        print(i)
+
+print("-----------------------------------------")
+
+sum=0
+for i in range(0,500000):
+    if i%2 is not 0:
+        sum+=i
+else:
+    print(sum)
+
+print("-----------------------------------------")
+
+for i in range(2018,0,-4):
+    print(i)
+
+print("-----------------------------------------")
+
+lowNum = 3
+highNum = 9
+mult=3
+for i in range(lowNum,highNum+1,mult):
+    print(i)

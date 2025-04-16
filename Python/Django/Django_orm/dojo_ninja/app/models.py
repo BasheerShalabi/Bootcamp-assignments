841261633b4b9c2b511211b0e5c8fb945e8dcd34
BasheerShalabi <shalabibasheer@gmail.com>
2025-04-16 10:57:50 +0300

    dojo ninjas templates

---
+++ Python/Django/Django_orm/dojo_ninja/app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=2)
+    ninja_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Ninja(models.Model):
+    dojo=models.ForeignKey(Dojo, related_name="ninjas", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+def add_dojo(post):
+    return Dojo.objects.create(name=post['name'],city=post['city'],state=post['state'])
+
+
+def get_dojo():
+    return Dojo.objects.all()
+
+def add_ninja(post):
+    dojo = Dojo.objects.get(id=post['dojo_id'])
+    dojo.ninja_count+=1
+    dojo.save()
+    return Ninja.objects.create(dojo=dojo,first_name=post['first_name'],last_name=post['last_name'])
+
+def get_ninja():
+    return Ninja.objects.all()
+
+def delete_dojo(post):
+    dojo = Dojo.objects.get(id=post['dojo_id'])
+    dojo.delete()
---
+++ Python/Django/Django_orm/dojo_ninja/app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from . import models
+
+def index(request):
+
+    context = {
+        'dojos':models.get_dojo(),
+        'ninjas': models.get_ninja()
+    }
+    
+    return render(request, 'index.html', context)
+
+def add_ninja(request):
+    if request.method == 'POST':
+        models.add_ninja(request.POST)
+        return redirect('/')
+    else:
+        return redirect('/')
+
+def add_dojo(request):
+    if request.method == 'POST':
+        models.add_dojo(request.POST)
+        return redirect('/')
+    else:
+        return redirect('/')
+
+def delete_dojo(request):
+    if request.method == 'POST':
+        models.delete_dojo(request.POST)
+        return redirect('/')
+    else:
+        return redirect('/')

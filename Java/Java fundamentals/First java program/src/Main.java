262fef753ae50638bfa2f96eb8d3cc78e4dcc6ea
BasheerShalabi <shalabibasheer@gmail.com>
2025-05-13 13:26:11 +0300

    first java

---
+++ Java/Java fundamentals/First java program/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("My name is Basheer Shalabi");
+        System.out.println("I am 26 years old");
+        System.out.println("My home town is Salfeet, Mas-ha");
+
+    }
+}

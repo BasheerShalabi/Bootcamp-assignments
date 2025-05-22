32ae4d2e6c3bbc11e8e161ee181660893c33af43
BasheerShalabi <shalabibasheer@gmail.com>
2025-05-22 13:54:36 +0300

    pathvariable

---
+++ Java/Java Spring/pathvariables/src/test/java/com/example/pathvariable/pathvariables/PathvariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathvariable.pathvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathvariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

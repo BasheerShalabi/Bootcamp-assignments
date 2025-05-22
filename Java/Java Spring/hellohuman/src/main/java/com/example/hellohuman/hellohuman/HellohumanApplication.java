7df352ff2f4c97f400167d0ab8b74804caa0c773
BasheerShalabi <shalabibasheer@gmail.com>
2025-05-22 13:44:24 +0300

    hellohuman

---
+++ Java/Java Spring/hellohuman/src/main/java/com/example/hellohuman/hellohuman/HellohumanApplication.java
@@ -0,0 +1,32 @@
+package com.example.hellohuman.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+public class HellohumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohumanApplication.class, args);
+	}
+
+	@RestController
+	public class HelloController{
+
+		@RequestMapping("/")
+		public String fullname(@RequestParam(value = "name",required = false,defaultValue = "Human") String name , @RequestParam(value = "last_name",required = false,defaultValue = "") String last_name ,@RequestParam(value = "times",required = false,defaultValue = "0") int times) {
+			String response = "Hello " + name + " " + last_name + "! ";
+			for (int i = 0; i < times; i++) {
+				 response += "Hello " + name + " " + last_name + "! ";
+			}
+			return response;
+		}
+		
+		
+	}
+
+}

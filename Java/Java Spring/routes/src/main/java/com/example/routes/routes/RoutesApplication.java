0addb8e511171790b1ac94537bee40159ace6f25
BasheerShalabi <shalabibasheer@gmail.com>
2025-05-22 13:23:04 +0300

    routes

---
+++ Java/Java Spring/routes/src/main/java/com/example/routes/routes/RoutesApplication.java
@@ -0,0 +1,42 @@
+package com.example.routes.routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@SpringBootApplication
+public class RoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/daikichi")
+	public class DaikichiController{
+
+		@GetMapping
+		public String index() {
+			return "Welcome!";
+		}
+
+		@GetMapping("/today")
+		public String today() {
+			return "Today you will find luck in all your endeavors!";
+		}
+
+		@GetMapping("/tomorrow")
+		public String tomorrow() {
+			return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas and experiences!";
+		}
+		
+
+		
+	}
+
+}
